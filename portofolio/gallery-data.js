fb3f1443dd64df6fd083cd28d7a023ae0093c594
Aku-Zumar <presidentpondok@gmail.com>
2026-02-28 08:45:26 +0700

    Add files via upload

---
+++ portofolio/gallery-data.js
@@ -0,0 +1,43 @@
+// Galeri coding-only:
+// 1) Simpan foto ke folder images/
+// 2) Tambahkan item di daftar di bawah ini
+//
+// Disarankan pakai object agar nama file & caption independen:
+// { src: "images/foto-kegiatan-01.jpg", caption: "Kegiatan A", alt: "Dokumentasi kegiatan A" }
+//
+// Jika menulis string saja:
+// "images/foto-kegiatan-01.jpg"
+// caption otomatis jadi "Foto 1", "Foto 2", dst (bukan nama file)
+
+window.portfolioGalleryItems = [
+  { src: "images/orspek.jpeg",             caption: "Kumpulan foto-foto di ORSPEK 2025", alt: "galeri 1" },
+  { src: "images/DSC00138.jpeg",           caption: "Foto ustadz Harno ikut sowan ke rumah ustadz Mundzir", alt: "galeri 2" },
+  { src: "images/aktiforganisasi.jpeg",    caption: "Kumpulan foto antusias dalam event", alt: "galeri 3" },
+  { src: "images/aku1.jpeg",               caption: "Menyanyikan mars MAPK di Bima Loka", alt: "galeri 4" },
+  { src: "images/aku2.jpeg",               caption: "Menyanyikan mars MAPK di Bima Loka", alt: "galeri 5" },
+  { src: "images/DSC00200.jpeg",             caption: "Belum", alt: "galeri 6" },
+  { src: "images/asrama.jpeg",             caption: "Belum", alt: "galeri 7" },
+  { src: "images/buku.jpeg",              caption: "Belum", alt: "galeri 8" },
+  { src: "images/cdr.jpeg",                caption: "Pembukaan CDR 2025", alt: "galeri 9" },
+  { src: "images/DSC06582.jpeg",             caption: "Penyambutan kehadiran ustadz Muchlis Hudaf", alt: "galeri 10" },
+  { src: "images/DSC00200.jpeg",           caption: "Sowan ke rumah ustadz Mundzir dan disambut hangat dengan...", alt: "galeri 11" },
+  { src: "images/DSC00712.jpeg",           caption: "Persipana salah satu event di MAPK", alt: "galeri 12" },
+  { src: "images/DSC00756.jpeg",           caption: "Pembacaan Maulid Simtu Ad-Durrar acara Isra Mikraj 2025", alt: "galeri 13" },
+  { src: "images/DSC00796.jpeg",           caption: "Mahallul Qiyam maulid Simtu Ad-Durrar Isra Mikraj 2025", alt: "galeri 15" },
+  { src: "images/DSC00851.jpeg",           caption: "Pembacaan doa Maulid Simtu Ad-Durrar", alt: "galeri 16" },
+  { src: "images/DSC00859.jpeg",           caption: "Seminar ORSPEK 2025", alt: "galeri 17" },
+  { src: "images/DSC00979.jpeg",           caption: "Brother Sharing ORSPEK 2025", alt: "galeri 18" },
+  { src: "images/DSC01256.jpeg",           caption: "Wide Game ORSPEK 2025", alt: "galeri 19" },
+  { src: "images/DSC01321.jpeg",           caption: "Wide Game ORSPEK 2025", alt: "galeri 20" },
+  { src: "images/DSC01329.jpeg",           caption: "Wide Game ORSPEK 2025", alt: "galeri 21" },
+  { src: "images/DSC01354.jpeg",           caption: "Wide Game ORSPEK 2025", alt: "galeri 22" },
+  { src: "images/DSC03422.jpeg",           caption: "Event seminar ORSPEK 2025", alt: "galeri 23" },
+  { src: "images/DSC03449.jpeg",           caption: "Seminar PENA 2025", alt: "galeri 24" },
+  { src: "images/DSC04913.jpeg",           caption: "Closing Muhadloroh dengan penerima penghargaan dari Divisi Tarbiyah", alt: "galeri 25" },
+  { src: "images/DSC04919.jpeg",           caption: "Foto bersama dengan adik-adik kelas 10 di Closing Muhadloroh sebagai penanda akhir kepengurusan", alt: "galeri 29" },
+  { src: "images/DSC04925.jpeg",           caption: "Foto panitia Closing Muhadloroh", alt: "galeri 31" },
+  { src: "images/DSC05529.jpeg",           caption: "Event besar tahunan MAPK Fair 2025", alt: "galeri 34" },
+  { src: "images/DSC06548.jpeg",           caption: "Dummy 04", alt: "galeri 35" },
+  { src: "images/DSC06551.jpeg",           caption: "Dummy 05", alt: "galeri 36" },
+  { src: "images/DSC06557.jpeg",           caption: "Dummy 07", alt: "galeri 38" },
+];
